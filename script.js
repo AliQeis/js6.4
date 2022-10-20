55ae59684f33e4aac2ecdb4922f590f98acec295
jschaik <9020512@student.zadkine.nl>
2022-10-20 13:57:26 +0200

    6.4 done

---
+++ script.js
@@ -0,0 +1,17 @@
+function calculator(num,num2){
+    let answere=num+num2;
+    console.log(answere)
+}
+calculator(8,15);
+
+function multiplybyfive(num){
+    let answere=num *5;
+    console.log(answere)
+}
+multiplybyfive(8);
+
+function calculateminutes(num){
+    let answere=num/60;
+    console.log(answere)
+}
+calculateminutes(60);
